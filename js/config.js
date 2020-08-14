01413ecde8dc4b397f661443add9f4d4c105e0c1
plumest <chayathon.k@ku.th>
2020-08-14 19:38:41 +0700

    Update config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_PMePZBufy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '256sc83umppprikp0o80ocn5vu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_gHYH75c3I', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '26n83rtpnrm5jovv4glvhqpore', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://cmf7rcl64h.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
